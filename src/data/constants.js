60fd0fed0158a8fff9e006e2398411cf4f64bc87
sneh1604 <22it140@charusat.edu.com>
2024-04-07 11:57:08 +0530

    resume added

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sneh1604",
   resume:
-    "#",
+    "https://drive.google.com/file/d/1l3n4uktM21Jf2Y7IoemsvvNo8Fvh9w96/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sneh-shah-4a534525a/",
   insta: "https://www.instagram.com/snehshah_5721/",
   facebook: "https://www.facebook.com/snehshah_5721/",
